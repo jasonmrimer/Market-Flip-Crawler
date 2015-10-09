6867bc5ab87b65cc92c60cdba8f92a45d6363adf
lovenirds <lovenirds@gmail.com>
2015-10-09 18:12:08 +0300

    Initialize project to GCloud

---
+++ MarketFlipCrawler/src/Main.java
@@ -0,0 +1,8 @@
+/*
+ * initialize project to GCloud.
+ * main will eventually run the threads of the site crawler and the database crawler.
+ * Jason M. Rimer, 20151009
+ */
+public class Main {
+
+}
