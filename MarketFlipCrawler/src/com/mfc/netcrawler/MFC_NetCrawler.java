d1c61c4bbcf0f01fb07ade20f05d075ff1ac8327
lovenirds <lovenirds@gmail.com>
2015-12-12 20:35:18 +0200

    I finished most of the unit tests for NetCrawler and move onto the integration testing.

--- MarketFlipCrawler/src/com/mfc/netcrawler/MFC_NetCrawler.java
+++ MarketFlipCrawler/src/com/mfc/netcrawler/MFC_NetCrawler.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
@@ -39,22 +40,40 @@ import com.google.appengine.repackaged.org.codehaus.jackson.map.RuntimeJsonMappi
  */
 public class MFC_NetCrawler implements Callable<MFC_NetCrawler> {
 
-	private final int			MAX_SITE_DEPTH	= 1;
 	private Document			siteDoc;
 	private Collection<String>	URLs;
 	private MFC_WebsiteDAO		database;
 	private String				startURLBeforeHash;
 	private String				startURLAfterHash;
+	private String				startURLBaseURI;
 
 	public MFC_NetCrawler() {
 		this.URLs = new ArrayList<String>();
 		this.siteDoc = null;
+		this.startURLBaseURI = "";
 	}
 
 	/**
-	 * This is the common interface to create the class that will be used
-	 * in the real program when connecting to a website database and having an
-	 * actual website URL.
+	 * This constructor is a mock object that operates outside of the useful system wherein it is
+	 * only given a local file path rather than a database connection and an URL from the Internet
+	 * (i.e., designed only for testing)
+	 * 
+	 * @param localFilePath
+	 */
+	public MFC_NetCrawler(String localFilePath) {
+		this();
+		/*
+		 * Uses the SHA-256 hashing algorithm to return a unique string for each URL. The database
+		 * stores each hashed string as the identifier for the URL to ensure it accounts for all
+		 * URLs regardless of length
+		 */
+		this.startURLBeforeHash = localFilePath;
+		this.startURLAfterHash = DigestUtils.sha256Hex(localFilePath);
+	}
+
+	/**
+	 * This is the common interface to create the class that will be used in the real program when
+	 * connecting to a website database and having an actual website URL.
 	 * 
 	 * @param database
 	 * @param startURL
@@ -65,50 +84,37 @@ public class MFC_NetCrawler implements Callable<MFC_NetCrawler> {
 	}
 
 	/**
-	 * This constructor is a mock object that operates outside of the useful system
-	 * wherein it is only given a local file path rather than a database connection and
-	 * an URL from the Internet (i.e., designed only for testing)
-	 * 
-	 * @param localFilePath
+	 * The purpose of this constructor is to serve as a mock object in order to test local files
+	 * with a URL in order to access the base URI to link to relative paths within the HTML
+	 * document.
+	 *
+	 * @param testFileURL
 	 */
-	public MFC_NetCrawler(String localFilePath) {
-		this();
-		/*
-		 * Uses the SHA-256 hashing algorithm to return a unique string for each URL.
-		 * The database stores each hashed string as the identifier for the URL to ensure
-		 * it accounts for all URLs regardless of length
-		 */
-		this.startURLBeforeHash = localFilePath;
-		this.startURLAfterHash = DigestUtils.sha256Hex(localFilePath);
+	public MFC_NetCrawler(String testFilePath, String testFileBaseURI) {
+		this(testFilePath);
+		this.startURLBaseURI = testFileBaseURI;
 	}
 
 	/**
-	 * Upon completion, this method will return a future to the its executor. In
-	 * particular, the source code of the website as a JSoup Document.
+	 * Upon completion, this method will return a future to the its executor. In particular, the
+	 * source code of the website as a JSoup Document.This method uses several JSoup tools to
+	 * extract the site document, links array, and content type to facilitate analysis and crawling.
+	 * It also checks the database to see if the program already examined the website to be
+	 * processed.
 	 */
 	@Override
 	public MFC_NetCrawler call() {
-		runJSoup();
+		createJSoupDocumentFromSite();
+		collectLinksFromSite();
+		catalogSiteVisit();
 		return this;
 	}
 
 	/**
-	 * This method uses several JSoup tools to extract the site document,
-	 * links array, and content type to facilitate analysis and crawling.
-	 * It also checks the database to see if the program already examined the
-	 * website to be processed.
+	 * This method catalogs the visit to a website by inserting the URL into a website database and
+	 * may evolve to include the date, time, etc.
 	 */
-	private void runJSoup() {
-		createJSDocument();
-		collectLinks();
-		catalogVisit();
-	}
-
-	/**
-	 * This method catalogs the visit to a website by inserting the
-	 * URL into a website database and may evolve to include the date, time, etc.
-	 */
-	private void catalogVisit() {
+	private void catalogSiteVisit() {
 		// Insert into database to catalog visit
 		if (database != null) {
 			database.insertURLToWebsiteTable(startURLAfterHash);
@@ -116,41 +122,58 @@ public class MFC_NetCrawler implements Callable<MFC_NetCrawler> {
 	}
 
 	/**
-	 * This method iterates through all the links in an HTML Doc and
-	 * adds the absolute references to the object's collection.
+	 * This method iterates through all the links in an HTML Doc and adds the absolute references to
+	 * the object's collection.
 	 */
-	private void collectLinks() {
-		// fetch the array of links
-		Elements links = siteDoc.select("a[href]");
-		// iterate each link inside the link array from the siteDoc
-		for (Element link : links) {
-			/*
-			 * Use a separate method to handle each link in order to catch errors specific to each
-			 * link rather than the overlapping URLs throughout the runJSoup method
-			 */
-			URLs.add(link.attr("abs:href"));
+	private void collectLinksFromSite() {
+		// Check whether the site provided a JSoup Doc
+		if (siteDoc != null) {
+			// fetch the array of links
+			Elements links = siteDoc.select("a[href]");
+			// iterate each link inside the link array from the siteDoc
+			for (Element link : links) {
+				/*
+				 * Use a separate method to handle each link in order to catch errors specific to
+				 * each link rather than the overlapping URLs throughout the runJSoup method
+				 * TODO what errors did i mean? @pastHB from @futureHB
+				 */
+				/*
+				 * I discovered through testing that there is some error i cannot figure out wherein
+				 * a local html file cannot return the relative reference and parse it into an
+				 * absolute reference. i may override this by concatenating my own references;
+				 * although, i prefer a more elegant solution.
+				 */
+				String linkString = link.attr("abs:href"); // ftr, neither this nor absUrl("href") works
+				if (linkString.isEmpty()) { // check if returned "" (i.e., the problem at hand)
+					URLs.add(siteDoc.baseUri() + link.attr("href")); // concatenate baseURI to relative ref
+					link.absUrl("href");
+				}
+				else { // for all the properly returned absolute refs
+					URLs.add(link.attr("abs:href"));
+				}
+			}
 		}
+		// If there was no Doc then it could extract no links
+		else URLs = null;
 	}
 
 	/**
-	 * The purpose of this method is to test the URL or path to a website or local document
-	 * and determine whether JSoup can properly extract a Document as well as catch all errors
-	 * the process throws.
+	 * The purpose of this method is to test the URL or path to a website or local document and
+	 * determine whether JSoup can properly extract a Document as well as catch all errors the
+	 * process throws.
 	 */
-	private void createJSDocument() {
+	private void createJSoupDocumentFromSite() {
 		try {
 			/*
 			 * From: http://jsoup.org/cookbook/input/load-document-from-url
 			 * [Jsoup.connect(StringURL)] only suports web URLs (http and https protocols); if you
-			 * need to load
-			 * from a file, use the parse(File in, String charsetName) method instead.
+			 * need to load from a file, use the parse(File in, String charsetName) method instead.
 			 */
 			if (startURLBeforeHash.startsWith("http://")) {
 				/*
-				 * TODO The tester limits the type of websites that the code
-				 * attempts to pull from in order to limit errors. We need to
-				 * expand this to a more elaborate testing scheme because this
-				 * is more like a "duct tape" solution for our temporary
+				 * TODO The tester limits the type of websites that the code attempts to pull from
+				 * in order to limit errors. We need to expand this to a more elaborate testing
+				 * scheme because this is more like a "duct tape" solution for our temporary
 				 * testing.
 				 */
 				// TODO ignoreContentType is part of the bad solution but gets
@@ -170,7 +193,7 @@ public class MFC_NetCrawler implements Callable<MFC_NetCrawler> {
 			}
 			// This block includes local files that are HTML docs (e.g., for mock objects)
 			else if (startURLBeforeHash.endsWith(".html")) {
-				siteDoc = Jsoup.parse(new File(startURLBeforeHash), "UTF-8");
+				siteDoc = Jsoup.parse(new File(startURLBeforeHash), "UTF-8", startURLBaseURI);
 			}
 			else siteDoc = null;
 		}
@@ -197,8 +220,26 @@ public class MFC_NetCrawler implements Callable<MFC_NetCrawler> {
 	}
 
 	/**
-	 * Getters: used to pass the source doc to the sourcecodanalyzer and the
-	 * URLs to iterate through in the manager
+	 * The purpose of this method is to return a string reprentation of the object that includes the
+	 * name of the object and important characteristics that can identify properties of the object:
+	 * whether it has a database connection and for what URL it was created.
+	 */
+	@Override
+	public String toString() {
+		String objectRepresentation;
+		if (database != null) {
+			objectRepresentation = "MFC_NetCrawler object with database for: " + startURLBeforeHash;
+		}
+		else {
+			objectRepresentation = "MFC_NetCrawler object without database for: "
+					+ startURLBeforeHash;
+		}
+		return objectRepresentation;
+	}
+
+	/**
+	 * Getters: used to pass the source doc to the sourcecodanalyzer and the URLs to iterate through
+	 * in the manager.
 	 * 
 	 * @return
 	 */
